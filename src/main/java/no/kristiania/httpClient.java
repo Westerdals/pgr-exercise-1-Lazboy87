6f02aab4b27d51c7d4ff62c9d8c1ddc9562de610
LASSE <lassehovden@gmail.com>
2019-09-11 15:54:08 +0200

    Client working

--- src/main/java/no/kristiania/httpClient.java
+++ src/main/java/no/kristiania/httpClient.java
@@ -1,12 +1,27 @@
 package no.kristiania;
 
+import java.io.IOException;
 import java.net.Socket;
 
-public class httpClient {
-  public static void main(String[] args) {
-    Socket socket=new Socket("urlecho.appspot.com",80);
+public class httpClient
+{
+
+
+  public static void main(String[] args)throws IOException
+  {
+    Socket socket = new Socket("urlecho.appspot.com", 80);
+    socket.getOutputStream().write("GET /echo?status200&Content-Type=text%2Fhtml&body=Goodbye%20world! HTTP/1.1\r\n".getBytes());
+    socket.getOutputStream().write("Host: urlecho.appspot.com\r\n".getBytes());
+    socket.getOutputStream().write("Connection: Close\r\n".getBytes());
+    socket.getOutputStream().write("\r\n".getBytes());
+    socket.getOutputStream().flush();
+
+    int c;
+    while ((c = socket.getInputStream().read()) !=-1){
+
+      System.out.print((char) c);
+    }
   }
+
+
 }
-
-
-
--- src/test/java/SimpleHTTPServerTest.java
+++ src/test/java/SimpleHTTPServerTest.java
@@ -2,9 +2,6 @@ import org.junit.jupiter.api.Test;
 
 public class SimpleHTTPServerTest {
 
-@Test void SimpleMathreq(){
-  assertEquals()
-}
 
 
 }
