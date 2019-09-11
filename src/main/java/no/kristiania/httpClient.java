362e0c3a5dc7f941571d62b2c7d7bf3078804ae8
LASSE <lassehovden@gmail.com>
2019-09-11 16:31:00 +0200

    added test

--- src/main/java/no/kristiania/httpClient.java
+++ src/main/java/no/kristiania/httpClient.java
@@ -5,13 +5,23 @@ import java.net.Socket;
 
 public class httpClient
 {
+private String host;
+  private int statusCode;
 
+  public httpClient(String host){
+ this.host=host;
+}
 
   public static void main(String[] args)throws IOException
-  {
-    Socket socket = new Socket("urlecho.appspot.com", 80);
+   {
+     new httpClient("urlecho.appspot.com").executeRequest();
+   }
+
+  public void executeRequest() throws IOException {
+
+    Socket socket = new Socket(host, 80);
     socket.getOutputStream().write("GET /echo?status200&Content-Type=text%2Fhtml&body=Goodbye%20world! HTTP/1.1\r\n".getBytes());
-    socket.getOutputStream().write("Host: urlecho.appspot.com\r\n".getBytes());
+    socket.getOutputStream().write(("Host: " + host + "\r\n").getBytes());
     socket.getOutputStream().write("Connection: Close\r\n".getBytes());
     socket.getOutputStream().write("\r\n".getBytes());
     socket.getOutputStream().flush();
@@ -24,4 +34,11 @@ public class httpClient
   }
 
 
+  public int getStatusCode() {
+    return statusCode;
+  }
+
+  public void setStatusCode(int statusCode) {
+    this.statusCode = statusCode;
+  }
 }
--- src/test/java/SimpleHTTPServerTest.java
+++ src/test/java/SimpleHTTPServerTest.java
@@ -1,7 +1,18 @@
+import no.kristiania.httpClient;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleHTTPServerTest {
 
+@Test
+  void shouldReadStatusCode()throws IOException {
+  httpClient client = new httpClient("urlecho.appspot.com");
+  client.executeRequest();
+  assertEquals(200,client.getStatusCode());
 
 
+}
 }
